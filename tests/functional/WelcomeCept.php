7cde3da4ae0acf0bae7ab2dc6f84cecaaef82ec6
David Ives <david@crowdcube.com>
2015-05-13 21:02:14 +0100

    Upgraded Laravel to 5.1

--- tests/functional/WelcomeCept.php
+++ tests/functional/WelcomeCept.php
@@ -5,7 +5,5 @@ $I->am('a staff member');
 $I->wantTo('test the DB is set up correctly');
 
 $I->amOnPage('/');
-
+$I->see('Holiday Manager');
 $I->seeInDatabase('users', ['id' => 1, 'first_name' => 'David']);
-
-

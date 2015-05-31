274ee50e504599efc58b3896ff7e1f779a4a46eb
David Ives <david@crowdcube.com>
2015-05-31 20:20:25 +0100

    Fixed broken JS paths

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,8 +1,8 @@
     <script src="/js/vendor/jquery.js"></script>
     <script src="/js/foundation/foundation.js"></script>
     <script src="/js/foundation/foundation.topbar.js"></script>
-    <script src="js/foundation/foundation.alert.js"></script>
-    <script src="js/foundation/foundation.reveal.js"></script>
+    <script src="/js/foundation/foundation.alert.js"></script>
+    <script src="/js/foundation/foundation.reveal.js"></script>
 
     <script>
         $(document).foundation();

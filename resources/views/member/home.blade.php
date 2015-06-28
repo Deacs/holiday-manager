04acc6e4b0630f81be9a7a185eb0d4b6f0fd98d1
David Ives <david@crowdcube.com>
2015-06-28 21:38:01 +0100

    Foundation Icons added

--- resources/views/member/home.blade.php
+++ resources/views/member/home.blade.php
@@ -6,20 +6,21 @@
         <script id="member-profile" type="x-template">
 
             <div class="large-2 columns">
-                <img src="@{{ member | getAvatar '150' }}">
+                <img src="@{{ member | getAvatar '200' }}">
             </div>
             <div class="large-6 columns">
                 <h2 v-text="member | nameFormat"></h2>
-                <h3><a href="@{{ member.department.url }}" v-text="member.department.name"></a> / <a href="@{{ member.location.name }}" v-text="member.location.name"></a></h3>
-                <h4 v-text="member.role"></h4>
+                <h5><i class="fi-torso large"></i> @{{ member.role }}</h5>
+                <h6><i class="fi-torsos-all large"></i> <a href="@{{ member.department.url }}" v-text="member.department.name"></a></h6>
+                <h6><i class="fi-compass"></i> <a href="@{{ member.location.name }}" v-text="member.location.name"></a></h6>
             </div>
             <div class="large-4 columns">
-                <h3>Contact</h3>
+                <h4>Contact</h4>
                 <ul class="no-bullet">
-                    <li><a href="mailto:@{{ member.email }}" v-text="member.email"></a></li>
-                    <li v-text="member.telephone"></li>
-                    <li v-text="member.extension"></li>
-                    <li><a href="skype:@{{ member.skype_name }}?call">Call</a> | <a href="skype:@{{ member.skype_name }}?chat">Chat</a></li>
+                    <li><i class="fi-mail large"></i> <a href="mailto:@{{ member.email }}" v-text="member.email"></a></li>
+                    <li><i class="fi-telephone large"></i> @{{ member.telephone }}</li>
+                    <li><i class="fi-thumbnails large"></i> @{{ member.extension }}</li>
+                    <li><i class="fi-social-skype large"></i> <a href="skype:@{{ member.skype_name }}?call">Call</a> | <a href="skype:@{{ member.skype_name }}?chat">Chat</a></li>
                 </ul>
             </div>
 

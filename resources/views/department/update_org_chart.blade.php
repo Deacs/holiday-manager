c1046f1425580d31c08f39b0ed6f965e30ee1473
David Ives <david@crowdcube.com>
2016-01-16 20:57:51 +0000

    SweetAlert integrated with Org Chart uploader

--- resources/views/department/update_org_chart.blade.php
+++ resources/views/department/update_org_chart.blade.php
@@ -1,4 +1,3 @@
-
 <form method="POST" action="/departments/{{ $department->slug }}/org-chart" class="dropzone" id="department-org-chart-upload">
     {{ csrf_field() }}
 </form>
@@ -9,8 +8,23 @@
             paramName: "file",
             maxFilesize: 2, // MB
             maxFiles: 1, // Do not allow multiple uploads
-            dictDefaultMessage: 'Drag your new organisation chart here'
+            dictDefaultMessage: 'Drag your new organisation chart here',
+
+            init: function() {
+                this.on("success", function(file) {
+                    swal({
+                        type: "success",
+                        title: "Success",
+                        text: "Organisational Chart successfully updated",
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+
+                    // Close the upload panel
+
+                    // Update with new image
+                });
+            }
         };
     </script>
 @endsection
-
